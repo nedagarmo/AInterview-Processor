c1ceba2318ee28a9b7f15a8cf282149e0d84e49b
Nelson Garzón <nedagarmo@gmail.com>
2020-12-28 10:56:58 -0500

    refactor: se desacopla la funcionalidad de videollamada en otro servicio

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,3 @@
 
 
 NAMESPACE = "webrtc"
-DATABASE = "postgresql://postgres:9084ZNjCJzBWsn2wFKK5@" \
-           "logical-databases.c1fxzrljft9f.us-east-2.rds.amazonaws.com:5432/prod_ainterview"
--- server.py
+++ server.py
@@ -4,95 +4,31 @@ import os
 import sys
 
 from flask import Flask
-from flask_migrate import Migrate
-from flask_socketio import SocketIO, join_room, emit
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects import postgresql
+from flask_socketio import SocketIO, emit
 
 from app.base.application import ModelEngine
 from app.features.emotions import FaceEmotionRecognitionModel
-from app.features.rooms import RoomsManager
-from app.settings import DATABASE
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.debug = False
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 io = SocketIO(app, cors_allowed_origins="*")
 engine = ModelEngine()
 
 
-class Room(db.Model):
-    __tablename__ = 'room'
-
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String())
-    participants = db.Column(postgresql.JSONB)
-
-    def __init__(self, code, participants):
-        self.code = code
-        self.participants = participants
-
-    def __repr__(self):
-        return f"<Room {self.code}>"
-
-
-rooms = RoomsManager(Room, db)
-
-
-@io.on("join")
-def handle_join(payload):
-    token = payload.get("token")
-    room = payload.get("room")
-    log('Received request to create or join room ' + payload.get("room"))
-
-    participants = len(rooms.participants(room))
-    log('Room ' + room + ' now has ' + str(participants) + ' client(s)')
-
-    if participants == 0:
-        join_room(room)
-        rooms.join(token, room)
-        log('Client ID ' + str(token) + ' created room ' + room)
-        emit("created", room)
-    elif participants == 1:
-        log('Client ID ' + str(token) + ' joined room ' + room)
-        join_room(room)
-        rooms.join(token, room)
-        emit('joined', room)
-        emit('join', room, room=room, include_self=False)
-    else:
-        emit('full', room)
-
-
-def log(*args):
-    array = ['Message from server:']
-    array.extend(args)
-    emit('log', array)
-
-
-@io.on("message")
-def handle_message(payload):
-    token = payload.get("token", None)
-    room = rooms.get_room(token)
-    log('message to room: ', room)
-    if room is not None:
-        message = payload.get("message", None)
-        log('Client said: ', message)
-        emit('message', message, room=room)
-
-
 @io.on("frame")
 def handle_frame(payload):
-    if not isinstance(payload, str):
+    if not isinstance(payload, dict):
         return
+
+    token = payload.get("token")
+    frame_string = payload.get("frame")
 
     frame = None
 
     try:
-        frame = payload.split(',')[1]
+        frame = frame_string.split(',')[1]
     except IndexError:
         pass
 
